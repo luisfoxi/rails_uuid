1d13259d967bab7ff961d5d59f13c1b8a527f386
luisfoxi <luisfoxi@hotmail.com>
2021-04-09 11:43:07 -0300

    [*] Testando o allPosts.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,7 +2,6 @@ module Types
   class QueryType < Types::BaseObject
     # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
     include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
@@ -13,5 +12,12 @@ module Types
     def test_field
       "Hello World!"
     end
+
+    field :all_posts, [PostType], null: false
+
+    # this method is invoked, when `all_link` fields is being resolved
+    def all_posts
+      Post.all
+    end 
   end
 end
