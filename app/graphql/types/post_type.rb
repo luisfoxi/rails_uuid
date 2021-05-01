79f5f63381b70d8f0aaa8cdece3349a386ea0e33
luisfoxi <luisfoxi@hotmail.com>
2021-05-01 18:50:56 -0300

    [=] Teste com field com parâmetros.

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -3,6 +3,14 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: true   # References: https://stackoverflow.com/questions/47960194/graphql-ruby-date-or-datetime-type
-    field :created_at, GraphQL::Types::ISO8601Date, null: false
+    field :created_at, String, null: false
+
+    def created_at(unit)
+      if unid == 'day'
+        created_at.day
+      else
+        created_at.to_s
+      end
+    end
   end
 end
