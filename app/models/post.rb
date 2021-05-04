36e9b5e3b1b2d3409574a6e33cecfeeb0b7fb365
luisfoxi <luisfoxi@hotmail.com>
2021-05-04 20:07:03 -0300

    [=] Teste de field com parâmetros.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
-    has_many :comments
+  has_many :comments
+  def created_at_part(unit: String)
+    if unit == 'day'
+      created_at.day.to_s
+    else
+      created_at.year
+    end
+  end 
 end
