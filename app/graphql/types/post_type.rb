0eed8f42c0cb159acc63c3405156e2237d9fb1e5
luisfoxi <luisfoxi@hotmail.com>
2021-05-06 22:24:12 -0300

    [=] Teste de field com parâmetros.

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -4,7 +4,9 @@ module Types
     field :title, String, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: true   # References: https://stackoverflow.com/questions/47960194/graphql-ruby-date-or-datetime-type
     # field :created_at, String, null: false
-    # field :created_at_part, Integer, null: true, 
-    field :created_at_part, resolver:CreatedAtPartDay  ########## CRIAR CLASSE PARA ESSE RESOLVER. https://hashrocket.com/blog/posts/graphql-way-limited-edition
+    # field :created_at_part, Integer, null: true do
+    #   argument :unit, Integer, required: false, default_value: 123
+    # end
+    # field :created_at_part, resolver:CreatedAtPartDay  ########## CRIAR CLASSE PARA ESSE RESOLVER. https://hashrocket.com/blog/posts/graphql-way-limited-edition
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord
   has_many :comments
-  def created_at_part(unit: String)
-    if unit == 'day'
-      created_at.day.to_s
-    else
-      created_at.year
-    end
-  end 
+  # def created_at_part(unit)
+  #   if unit == 'day'
+  #     created_at.day.to_s
+  #   else
+  #     created_at.year
+  #   end
+  # end 
 end
